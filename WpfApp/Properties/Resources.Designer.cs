eaa7dacace88f95cd0085aa2df19b57cafa76668
Paweł Łęczycki <pawel@msd.info.pl>
2020-05-13 15:12:08 +0200

    Resources

--- WpfApp/Properties/Resources.Designer.cs
+++ WpfApp/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace WpfApp.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace WpfApp.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApp.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace WpfApp.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace WpfApp.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Data urodzenia.
         /// </summary>
-        internal static string BirthDate {
+        public static string BirthDate {
             get {
                 return ResourceManager.GetString("BirthDate", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace WpfApp.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Imię.
         /// </summary>
-        internal static string FirstName {
+        public static string FirstName {
             get {
                 return ResourceManager.GetString("FirstName", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace WpfApp.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Nazwisko.
         /// </summary>
-        internal static string LastName {
+        public static string LastName {
             get {
                 return ResourceManager.GetString("LastName", resourceCulture);
             }
