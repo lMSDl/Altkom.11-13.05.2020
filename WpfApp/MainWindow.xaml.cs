f0937006aec19729b05d42b88b3e05b777900e2c
Paweł Łęczycki <pawel@msd.info.pl>
2020-05-13 15:03:08 +0200

    Lista osób

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DAL.Services;
+using Models;
+using Service.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,18 +22,42 @@ namespace WpfApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private IService<Person> Service = new PersonService();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var button = sender as Button;
+        public ICollection<Person> People { get; set; }
+        public Person SelectedPerson { get; set; }
 
-            MessageBox.Show(button?.Content.ToString());
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            People = Service.Read();
+            PropertyChanged(this, new PropertyChangedEventArgs(nameof(People)));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedPerson == null)
+                return;
+            var dialog = new PersonWindow(SelectedPerson);
+            dialog.ShowDialog();
+            Service.Update(SelectedPerson);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedPerson == null)
+                return;
+            Service.Delete(SelectedPerson.GetType(), SelectedPerson.Id);
+            RefreshButton_Click(this, new RoutedEventArgs());
         }
     }
 }
